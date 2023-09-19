72bcd21ccdac392896170c36ba2fbace45debd5e
Ricardo Oliveira <r.deoliveira527@gmail.com>
2023-09-19 19:41:37 +0100

    fix header burger menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,9 +6,17 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark container-fluid">
       <Link className="navbar-brand" to="/">Ricardo Oliveira</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+      <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
