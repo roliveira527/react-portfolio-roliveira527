ab45bfba767493d5c49494a087b4cb6051cbef26
Ricardo Oliveira <r.deoliveira527@gmail.com>
2023-09-19 17:47:49 +0100

    implement header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
   return (
-    <nav>
-      <div> hello </div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
-    </nav>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark container-fluid">
+        <Link className="navbar-brand" to="/">Ricardo Oliveira</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/projects">Projects</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 }
 
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <nav>
-      hello
-    </nav>
+    <div className="container mt-5">
+      <h1>Welcome to Your Portfolio</h1>
+      <div className="row">
+        <div className="col-lg-6">
+          <img src="path_to_headshot.jpg" alt="Headshot" className="img-fluid rounded-circle mt-4" />
+        </div>
+        <div className="col-lg-6">
+          <p>Your brand statement</p>
+          <p>This is your portfolio site</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
