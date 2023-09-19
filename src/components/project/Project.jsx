cd48d32949fb6aaa9f307514f98e16f48c2bafeb
Ricardo Oliveira <r.deoliveira527@gmail.com>
2023-09-19 18:54:03 +0100

    implement Projects and Project components

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Project.css';
 
-const Project = () => {
+const Project = (props) => {
+  const { title, deployedLink, githubLink, screenshot } = props;
+
   return (
-    <nav>
-      hello
-    </nav>
+    <div className="col-lg-4 mb-4">
+      <div className="card">
+        {/* <img src={screenshot} className="card-img-top" alt={title} /> */}
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">
+            Deployed Version: <a href={deployedLink}>{deployedLink}</a>
+            <br />
+            GitHub Repository: <a href={githubLink}>{githubLink}</a>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Project from '../project/Project';
+import projectsData from './Projects_data.json';
+import './Projects.css';
 
 const Projects = () => {
   return (
-    <nav>
-      hello
-    </nav>
+    <div className="container mt-5">
+      <div className="row">
+        {projectsData.map((project, index) => (
+          <Project key={index} {...project} />
+        ))}
+      </div>
+    </div>
   );
 }
 
