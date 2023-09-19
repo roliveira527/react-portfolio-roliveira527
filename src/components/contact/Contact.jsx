fb0b90931bfbf80b8e1f039a0ba03fbb98bb80ac
Ricardo Oliveira <r.deoliveira527@gmail.com>
2023-09-19 19:47:26 +0100

    implement CV PDF and update Contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,8 +6,11 @@ const Contact = () => {
       <h2>Contact Information</h2>
       <p>GitHub: <a href="https://github.com/roliveira527">github.com/roliveira527</a></p>
       <p>LinkedIn: <a href="https://www.linkedin.com/in/ricardo-oliveira-cs/">linkedin.com/in/ricardo-oliveira-cs/</a></p>
-      <p>Email: your.email@example.com</p>
+      <p>Email: r.deoliveira527@gmail.com</p>
       <p>Phone: (123) 456-7890</p>
+      <p>CV: {"\n"}
+        <a href="src\assets\Ricardo_Oliveira_CV___1608.pdf" target="_blank" rel="noopener noreferrer">Download CV (PDF)</a>
+      </p>
     </div>
   );
 }
