b0cf69997bff1bfa37467a4aa0a45d478586a6b5
Ricardo Oliveira <r.deoliveira527@gmail.com>
2023-09-19 19:26:09 +0100

    fixed gutter spacing around page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,24 +4,22 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark container-fluid">
-        <Link className="navbar-brand" to="/">Ricardo Oliveira</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/projects">Projects</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark container-fluid">
+      <Link className="navbar-brand" to="/">Ricardo Oliveira</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/projects">Projects</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
