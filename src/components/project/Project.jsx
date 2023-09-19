e2e70063c476442b5ad1e56437722afdefaaf6f9
Ricardo Oliveira <r.deoliveira527@gmail.com>
2023-09-19 20:24:36 +0100

    populate JSON with project data

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -7,7 +7,7 @@ const Project = (props) => {
   return (
     <div className="col-lg-4 mb-4">
       <div className="card">
-        {/* <img src={screenshot} className="card-img-top" alt={title} /> */}
+        <img src={screenshot} className="card-img-top" alt={title} />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">
